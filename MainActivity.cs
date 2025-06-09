72df0ddcc35e8e66147c8945fbb8bce2525a046f
charles-lao <charles.h.lao@gmail.com>
2025-06-10 11:45:05 +1200

    Complete Section 8: UI Deep Dive

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,7 +1,9 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using AndroidX.AppCompat.App;
+using Calligraphy;
 
 namespace MoneyAppXamarin
 {
@@ -11,10 +13,20 @@ namespace MoneyAppXamarin
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            
+
+            //CalligraphyConfig.InitDefault(new CalligraphyConfig.Builder()
+            //    .SetDefaultFontPath("fonts/GlacialIndifference-Regular.ttf")
+            //    .SetFontAttrId(Resource.Attribute.fontPath)
+            //    .Build());
+
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
         }
+
+        //protected override void AttachBaseContext(Context @base)
+        //{
+        //    base.AttachBaseContext(CalligraphyContextWrapper.Wrap(@base));
+        //}
         
     }
 }
